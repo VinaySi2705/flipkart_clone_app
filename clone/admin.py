f4cb804a0336cb76de87ccc3980aca7538e024c0
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-02 18:02:04 +0530

    Message

--- clone/admin.py
+++ clone/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from . models import Product
+ # Category
 
 admin.site.register(Product)
+# admin.site.register(Category)
--- clone/models.py
+++ clone/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural="Categories"
 class Product(models.Model):
-    categories = models.CharField(max_length=50)
     items = models.CharField(max_length=50)
+    #category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     image = models.CharField(max_length=50)
     price = models.IntegerField()
 
