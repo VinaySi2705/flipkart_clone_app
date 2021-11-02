933ceb89b48b792c99c8a80e049973478afa6a24
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-02 15:22:49 +0530

    Message

--- clone/urls.py
+++ clone/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from . import views
+from django.conf.urls import url,include
+from clone.views import dashboard
+from clone import views
 urlpatterns = [
-    path('', views.Secr.home),
-    path('categories', views.Secr.categories),
-    path('products/', views.Secr.all_products),
-
-
+    # path('', views.Secr.home),
+    path('',views.home),
+    #path('categories', views.Secr.categories),
+    path('categories',views.categories),
+    # path('products/', views.Secr.products),
+    path('products/',views.products),
+    url(r"^accounts/",include("django.contrib.auth.urls")),
+    # url(r"^dashboard/", dashboard, name="dashboard"),
+    url(r"^dashboard/",dashboard,name="dashboard"),
 ]
--- clone/views.py
+++ clone/views.py
@@ -1,31 +1,44 @@
 from django.shortcuts import render
 from clone.models import Product
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-class LockedView(LoginRequiredMixin):
-    login_url = "admin:login"
+def dashboard(request):
+    return render(request, "clone/dashboard.html")
 
+def categories(request):
+        products = Product.objects.all()
+        return render(request, 'clone/categories.html', {'products': products})
 
-class Secr(LockedView):
-    def all_products(request):
-        if request.user.is_authenticated:
-            products = Product.objects.all()
-            return render(request, 'clone/products.html', {'products': products})
-        else:
-            return HttpResponse("Please login again")
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'clone/products.html', {'products': products})
 
-    def home(request):
-        if request.user.is_authenticated:
-            return render(request, 'clone/base.html')
-        else:
-            return HttpResponse("Please login again")
+def home(request):
+    return render(request, 'clone/homepage.html')
 
-    def categories(request):
-        if request.user.is_authenticated:
-            products = Product.objects.all()
-            return render(request, 'clone/categories.html', {'products': products})
-        else:
-            return HttpResponse("Please login again")
+# class LockedView(LoginRequiredMixin):
+#     login_url = "admin:login"
+
+# class Secr(LockedView):
+#     def products(request):
+#         if request.user.is_authenticated:
+#             products = Product.objects.all()
+#             return render(request, 'clone/products.html', {'products': products})
+#         else:
+#             return HttpResponse("Please login again")
+#
+#     def home(request):
+#         if request.user.is_authenticated:
+#             return render(request, 'clone/homepage.html')
+#         else:
+#             return HttpResponse("Please login again")
+
+    # def categories(request):
+    #     if request.user.is_authenticated:
+    #         products = Product.objects.all()
+    #         return render(request, 'clone/categories.html', {'products': products})
+    #     else:
+    #         return HttpResponse("Please login again")
