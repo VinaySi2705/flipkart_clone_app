54b2bd476edea40662ba931a71384f253579bd16
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-08 11:19:14 +0530

    Message

--- clone/urls.py
+++ clone/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . views import Index, Signup, Login, logout, Cart, Checkout, OrderView
-from clone.middlewares.auth import auth_middleware
+
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', Signup.as_view(), name='signup'),
@@ -8,5 +8,5 @@ urlpatterns = [
     path('logout', logout, name='Logout'),
     path('cart', Cart.as_view(), name='cart'),
     path('check-out', Checkout.as_view(), name='checkout'),
-    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+    path('orders', OrderView.as_view(), name='orders'),
 ]
