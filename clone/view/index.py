8a688388f1656f8c09f195eea2d09ea1b3167fa1
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-08 18:17:14 +0530

    Message

--- clone/view/index.py
+++ clone/view/index.py
@@ -2,7 +2,6 @@ from django.views import View
 from clone.models import Category, Product
 from django.shortcuts import redirect, render
 
-
 class Index(View):
     def post(self, request):
         product = request.POST.get('product')
