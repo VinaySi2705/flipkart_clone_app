59f0b6c44671ba80acb0edb28083ba175ee3a5ea
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-11 11:30:28 +0530

    added authorizations for not logged in user

--- clone/urls.py
+++ clone/urls.py
@@ -6,15 +6,14 @@ from clone.view.checkout import Checkout
 from clone.view.login import Login
 from clone.view.cartview import Cart
 from clone.view.ordered import OrderView
-
+from clone.middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout, name='Logout'),
-    path('cart', Cart.as_view(), name='cart'),
-    path('check-out', Checkout.as_view(), name='checkout'),
-    path('orders', OrderView.as_view(), name='orders'),
-
+    path('cart',auth_middleware(Cart.as_view()), name='cart'),
+    path('check-out',auth_middleware(Checkout.as_view()), name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
 ]
