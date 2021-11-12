ee00de493dd8415d7799634761ba84d957627a6c
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-12 13:57:46 +0530

    added functionality to remove all items form cart and some visual improvements at signup and login page

--- clone/view/cartview.py
+++ clone/view/cartview.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from clone.models import Product
 from django.views import View
 
@@ -9,11 +9,18 @@ class Cart(View):
         products = Product.get_products_by_id(ids)
         return render(request, 'cart.html', {'products': products})
 
-    def post(self,request):
-        remove_item=request.POST.get('remove_item')
-        cart=request.session.get('cart')
-        cart.pop(remove_item)
-        request.session['cart']=cart
-        ids=list(request.session.get('cart').keys())
-        products=Product.get_products_by_id(ids)
-        return render(request,'cart.html',{'products':products})
+    def post(self, request):
+        remove_item = request.POST.get('remove_item')
+        clear = request.POST.get('clear')
+        if remove_item:
+            cart = request.session.get('cart')
+            cart.pop(remove_item)
+            request.session['cart'] = cart
+            ids = list(request.session.get('cart').keys())
+            products = Product.get_products_by_id(ids)
+            return render(request, 'cart.html', {'products': products})
+
+        elif clear:
+            request.session.get('cart').clear()
+            request.session['cart'] = {}
+            return redirect('cart')
