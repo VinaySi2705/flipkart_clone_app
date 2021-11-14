d6710327379537d7899cebd8e2eeffa5f55dd477
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-11-14 12:38:48 +0530

    Litle changes in admin paneel

--- clone/admin.py
+++ clone/admin.py
@@ -4,14 +4,16 @@ from . models import Product, Category, Customer, Order
 
 
 class AdminProduct(admin.ModelAdmin):
-    list_display = ['name', 'price', 'category']
+    list_display = ['name', 'price','quantity', 'category']
 
 
 class AdminCategory(admin.ModelAdmin):
     list_display = ['name']
 
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name','last_name','phone','email']
 
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category, AdminCategory)
-admin.site.register(Customer)
+admin.site.register(Customer, AdminCustomer)
 admin.site.register(Order)
